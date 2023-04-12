38fd9de5b9f2beb200eb9a21c938e06b3deb72cb
Vítor Gabriel <vggd@cin.ufpe.br>
2023-04-12 20:05:23 -0300

    update na main

--- main.R
+++ main.R
@@ -4,88 +4,27 @@ library(dplyr)
 
 data <- read.csv("AnaliseGas.csv")
 
-# Define UI
+#definindo a Interface de Usuário
 ui <- fluidPage(
-  # TITULO DO APP
+  #titulo da pagina
   titlePanel("Análise do preço da gasolina"),
-  # Barra lateral com os inputs
+  
   sidebarLayout(
-    # painel da Barra Lateral
     sidebarPanel(
-      # Input de Seleção de estados para Análise
-      selectInput(
-        inputId = "y",
-        label = "Estados",
-        choices = c("Norte", "Nordeste", "Sul", "Sudeste", "Centro-Oeste"),
-        selected = "Norte"
-      ),
-      # Input de Seleção de quais produtos Analisar
-      checkboxGroupInput(
-        inputId = "PRODUTO",
-        label = "Produto", 
-        choices = c("Etanol Hidratado", "Gasolina Comum", "GLP", "GNV", "Óleo Diesel", "Óleo Diesel S10"),
-        selected = c("Gasolina Comum", "GLP")
-      ),
-      # Input com as datas iniciais e finais para Análise
-      dateInput("start_date", "Data Inicial:", value = min(data$DATA_INICIAL)),
-      dateInput("end_date", "Data Final:", value = max(data$DATA_INICIAL)),
-      # Botão para atualizar os gráficos
-      actionButton("update_button", "Atualizar")
-    ),
-    
-    #painel principal
-    mainPanel(
-      # Output: Tabset w/ plot, summary, and table ----
-      #tabsetPanel(type = "tabs",
-                  #tabPanel("Linha", plotOutput("line_chart")),
-                  #tabPanel("Histograma", plotOutput("histogram"))
-                  
-      tags$h3("Gráfico de Linha"),
-      plotOutput("line_chart"),
-      tags$h3("Histograma"),
-      plotOutput("histogram")
+      fluidPage(
+        # Caixa Seletora dos estados
+        selectInput("regiao", label = h3("Região"), 
+                    choices = list("Norte" = 1, "Nordeste" = 2, "Centro-Oeste" = 3, "Sudeste" = 4, "Sul" = 5), 
+                    selected = 1),
+      )
     )
-  )
+  )  
 )
 
-server <- function(input, output){
+server <- function(input, output) {
   
-  filtered_data <- reactive({
-    data %>%
-      filter(DATA_INICIAL >= input$DATA_INICIAL & DATA_INICIAL <= input$DATA_INICIAL) %>%
-      filter(PRODUTO %in% input$PRODUTO)
-    
-  })
-  
-  plot_line_chart <- function(data) {
-    ggplot(data, aes(x = DATA_INICIAL, y = !!sym(input$y))) +
-      geom_line() +
-      labs(x = "Data", y = "Preço da moeda") +
-      theme_minimal()
-  }
-  
-  output$line_chart <- renderPlot({
-    plot_line_chart(filtered_data())
-  })
-  
-  output$histogram <- renderPlot({
-    ggplot(filtered_data(), aes(x = !!sym(input$y))) +
-      geom_histogram(bins = 30) +
-      labs(x = "Preço da moeda", y = "Frequência") +
-      theme_minimal()
-  })
-  
-  observeEvent(input$update_button, {
-    output$line_chart <- renderPlot({
-      plot_line_chart(filtered_data())
-    })
-    output$histogram <- renderPlot({
-      ggplot(filtered_data(), aes(x = !!sym(input$y))) +
-        geom_histogram(bins = 30) +
-        labs(x = "Preço da moeda", y = "Frequência") +
-        theme_minimal()
-    })
-  })
+  # You can access the value of the widget with input$select, e.g.
+  output$value <- renderPrint({ input$regiao })
   
 }
 
