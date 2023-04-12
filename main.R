85a0df87d9b34d419503c8818b846a91e9e5eb0b
Vítor Gabriel <vggd@cin.ufpe.br>
2023-04-12 19:42:06 -0300

    att Vitola

--- main.R
+++ main.R
@@ -2,29 +2,44 @@ library(shiny)
 library(ggplot2)
 library(dplyr)
 
-data <- read.csv("crypto-markets.csv")
+data <- read.csv("AnaliseGas.csv")
 
+# Define UI
 ui <- fluidPage(
-  titlePanel("Dashboard do CSV Crypto Markets"),
+  # TITULO DO APP
+  titlePanel("Análise do preço da gasolina"),
+  # Barra lateral com os inputs
   sidebarLayout(
+    # painel da Barra Lateral
     sidebarPanel(
+      # Input de Seleção de estados para Análise
       selectInput(
         inputId = "y",
-        label = "eixo-y",
-        choices = c("volume", "close"),
-        selected = "close"
+        label = "Estados",
+        choices = c("Norte", "Nordeste", "Sul", "Sudeste", "Centro-Oeste"),
+        selected = "Norte"
       ),
+      # Input de Seleção de quais produtos Analisar
       checkboxGroupInput(
-        inputId = "name",
-        label = "Select Currency", 
-        choices = c("Bitcoin", "Ethereum", "Litecoin"),
-        selected = c("Bitcoin", "Ethereum", "Litecoin")
+        inputId = "PRODUTO",
+        label = "Produto", 
+        choices = c("Etanol Hidratado", "Gasolina Comum", "GLP", "GNV", "Óleo Diesel", "Óleo Diesel S10"),
+        selected = c("Gasolina Comum", "GLP")
       ),
-      dateInput("start_date", "Data Inicial:", value = min(data$date)),
-      dateInput("end_date", "Data Final:", value = max(data$date)),
+      # Input com as datas iniciais e finais para Análise
+      dateInput("start_date", "Data Inicial:", value = min(data$DATA_INICIAL)),
+      dateInput("end_date", "Data Final:", value = max(data$DATA_INICIAL)),
+      # Botão para atualizar os gráficos
       actionButton("update_button", "Atualizar")
     ),
+    
+    #painel principal
     mainPanel(
+      # Output: Tabset w/ plot, summary, and table ----
+      #tabsetPanel(type = "tabs",
+                  #tabPanel("Linha", plotOutput("line_chart")),
+                  #tabPanel("Histograma", plotOutput("histogram"))
+                  
       tags$h3("Gráfico de Linha"),
       plotOutput("line_chart"),
       tags$h3("Histograma"),
@@ -37,13 +52,13 @@ server <- function(input, output){
   
   filtered_data <- reactive({
     data %>%
-      filter(date >= input$start_date & date <= input$end_date) %>%
-      filter(name %in% input$name)
+      filter(DATA_INICIAL >= input$DATA_INICIAL & DATA_INICIAL <= input$DATA_INICIAL) %>%
+      filter(PRODUTO %in% input$PRODUTO)
     
   })
   
   plot_line_chart <- function(data) {
-    ggplot(data, aes(x = date, y = !!sym(input$y))) +
+    ggplot(data, aes(x = DATA_INICIAL, y = !!sym(input$y))) +
       geom_line() +
       labs(x = "Data", y = "Preço da moeda") +
       theme_minimal()
