b777e6abaa381c82d84a4af82e1f102eaf47124a
guilp03 <108680176+guilp03@users.noreply.github.com>
2023-04-12 21:23:54 -0300

    mudando nome da data e incrementando codigo

--- main.R
+++ main.R
@@ -12,23 +12,23 @@ ui <- fluidPage(
   sidebarLayout(
     # painel da Barra Lateral
     sidebarPanel(
-      # Input de Seleção de estados para Análise
+      # Input de Seleção de regioes para Análise
       selectInput(
-        inputId = "y",
-        label = "Estados",
-        choices = c("Norte", "Nordeste", "Sul", "Sudeste", "Centro-Oeste"),
-        selected = "Norte"
+        inputId = "REGIÃO",
+        label = "REGIOES",
+        choices = c("NORTE", "NORDESTE", "SUL", "SUDESTE", "CENTRO OESTE"),
+        selected = "NORTE"
       ),
       # Input de Seleção de quais produtos Analisar
       checkboxGroupInput(
         inputId = "PRODUTO",
         label = "Produto", 
-        choices = c("Etanol Hidratado", "Gasolina Comum", "GLP", "GNV", "Óleo Diesel", "Óleo Diesel S10"),
-        selected = c("Gasolina Comum", "GLP")
+        choices = c("ETANOL HIDRATADO", "GASOLINA COMUM", "GLP", "GNV", "ÓLEO DIESEL", "ÓLEO DIESEL S10"),
+        selected = c("GASOLINA COMUM", "GLP")
       ),
       # Input com as datas iniciais e finais para Análise
-      dateInput("start_date", "Data Inicial:", value = min(data$DATA_INICIAL)),
-      dateInput("end_date", "Data Final:", value = max(data$DATA_INICIAL)),
+      dateInput("start_date", "Data Inicial:", value = min(data$DATE)),
+      dateInput("end_date", "Data Final:", value = max(data$DATE)),
       # Botão para atualizar os gráficos
       actionButton("update_button", "Atualizar")
     ),
@@ -37,9 +37,9 @@ ui <- fluidPage(
     mainPanel(
       # Output: Tabset w/ plot, summary, and table ----
       #tabsetPanel(type = "tabs",
-                  #tabPanel("Linha", plotOutput("line_chart")),
-                  #tabPanel("Histograma", plotOutput("histogram"))
-                  
+      #tabPanel("Linha", plotOutput("line_chart")),
+      #tabPanel("Histograma", plotOutput("histogram"))
+      
       tags$h3("Gráfico de Linha"),
       plotOutput("line_chart"),
       tags$h3("Histograma"),
@@ -52,13 +52,13 @@ server <- function(input, output){
   
   filtered_data <- reactive({
     data %>%
-      filter(DATA_INICIAL >= input$DATA_INICIAL & DATA_INICIAL <= input$DATA_INICIAL) %>%
-      filter(PRODUTO %in% input$PRODUTO)
-    
+      filter(DATE >= input$start_date & DATE <= input$end_date) %>%
+      filter(PRODUTO %in% input$PRODUTO) %>%
+      filter(REGIÃO %in% input$REGIÃO)
   })
   
   plot_line_chart <- function(data) {
-    ggplot(data, aes(x = DATA_INICIAL, y = !!sym(input$y))) +
+    ggplot(data, aes(x = DATE, y = "PREÇO MÉDIO")) +
       geom_line() +
       labs(x = "Data", y = "Preço da moeda") +
       theme_minimal()
@@ -69,7 +69,7 @@ server <- function(input, output){
   })
   
   output$histogram <- renderPlot({
-    ggplot(filtered_data(), aes(x = !!sym(input$y))) +
+    ggplot(filtered_data(), aes(x = "PREÇO MÉDIO")) +
       geom_histogram(bins = 30) +
       labs(x = "Preço da moeda", y = "Frequência") +
       theme_minimal()
@@ -80,7 +80,7 @@ server <- function(input, output){
       plot_line_chart(filtered_data())
     })
     output$histogram <- renderPlot({
-      ggplot(filtered_data(), aes(x = !!sym(input$y))) +
+      ggplot(filtered_data(), aes(x = "PREÇO MÉDIO")) +
         geom_histogram(bins = 30) +
         labs(x = "Preço da moeda", y = "Frequência") +
         theme_minimal()
