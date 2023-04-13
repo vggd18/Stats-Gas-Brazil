1dae9a0cbad3a37c644fa28a6b1e5659434638af
guilp03 <108680176+guilp03@users.noreply.github.com>
2023-04-12 22:23:43 -0300

    aa

--- main.R
+++ main.R
@@ -2,7 +2,7 @@ library(shiny)
 library(ggplot2)
 library(dplyr)
 
-data <- read.csv("AnaliseGas.csv")
+data <- read.csv("BigmacPrice.csv")
 
 # Define UI
 ui <- fluidPage(
@@ -12,23 +12,21 @@ ui <- fluidPage(
   sidebarLayout(
     # painel da Barra Lateral
     sidebarPanel(
-      # Input de Seleção de regioes para Análise
-      selectInput(
-        inputId = "REGIÃO",
-        label = "REGIOES",
-        choices = c("NORTE", "NORDESTE", "SUL", "SUDESTE", "CENTRO OESTE"),
-        selected = "NORTE"
-      ),
       # Input de Seleção de quais produtos Analisar
+      selectInput(inputId = "name", 
+        label = "Choose a Country", 
+        choices = unique(data$name)), 
+        plotOutput("line"),
+      
       checkboxGroupInput(
-        inputId = "PRODUTO",
-        label = "Produto", 
-        choices = c("ETANOL HIDRATADO", "GASOLINA COMUM", "GLP", "GNV", "ÓLEO DIESEL", "ÓLEO DIESEL S10"),
-        selected = c("GASOLINA COMUM", "GLP")
+        inputId = "main",
+        label = "Select Currency", 
+        choices = c("Brazil", "USA", "China"),
+        selected = "Brazil"
       ),
       # Input com as datas iniciais e finais para Análise
-      dateInput("start_date", "Data Inicial:", value = min(data$DATE)),
-      dateInput("end_date", "Data Final:", value = max(data$DATE)),
+      dateInput("start_date", "Data Inicial:", value = min(data$date)),
+      dateInput("end_date", "Data Final:", value = max(data$date)),
       # Botão para atualizar os gráficos
       actionButton("update_button", "Atualizar")
     ),
@@ -52,15 +50,15 @@ server <- function(input, output){
   
   filtered_data <- reactive({
     data %>%
-      filter(DATE >= input$start_date & DATE <= input$end_date) %>%
-      filter(PRODUTO %in% input$PRODUTO) %>%
-      filter(REGIÃO %in% input$REGIÃO)
+      filter(date >= input$start_date & date <= input$end_date) %>%
+      filter(name %in% input$main)
   })
   
   plot_line_chart <- function(data) {
-    ggplot(data, aes(x = DATE, y = "PREÇO MÉDIO")) +
+    ggplot(data, aes(x = date, y = !!sym("dollar_price"), color = name, group = name)) +
+      geom_point() +
       geom_line() +
-      labs(x = "Data", y = "Preço da moeda") +
+      labs(x = "Data", y = "preco do Bigmac") +
       theme_minimal()
   }
   
@@ -69,7 +67,7 @@ server <- function(input, output){
   })
   
   output$histogram <- renderPlot({
-    ggplot(filtered_data(), aes(x = "PREÇO MÉDIO")) +
+    ggplot(filtered_data(), aes(x = date, y = !!sym("dollar_price")), color = "name", group = "name") +
       geom_histogram(bins = 30) +
       labs(x = "Preço da moeda", y = "Frequência") +
       theme_minimal()
@@ -80,7 +78,7 @@ server <- function(input, output){
       plot_line_chart(filtered_data())
     })
     output$histogram <- renderPlot({
-      ggplot(filtered_data(), aes(x = "PREÇO MÉDIO")) +
+      ggplot(filtered_data(), aes(x = date, y = !sym("dollar_price"))) +
         geom_histogram(bins = 30) +
         labs(x = "Preço da moeda", y = "Frequência") +
         theme_minimal()
